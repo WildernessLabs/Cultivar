62376ce74faca9e5f68670646ba9a58c32368682
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-09-14 00:34:41 -0700

    Update plot curves and cleanup

--- source/apps/Cultivar_reTerminal/ViewModels/MainWindowViewModel.cs
+++ source/apps/Cultivar_reTerminal/ViewModels/MainWindowViewModel.cs
@@ -8,48 +8,6 @@ namespace Cultivar_reTerminal.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private bool _isTemperatureChartVisible;
-        public bool IsTemperatureChartVisible
-        {
-            get => _isTemperatureChartVisible;
-            set
-            {
-                if (value)
-                {
-                    IsHumidityChartVisible = IsSoilMoistureChartVisible = false;
-                }
-                this.RaiseAndSetIfChanged(ref _isTemperatureChartVisible, value);
-            }
-        }
-
-        private bool _isHumidityChartVisible;
-        public bool IsHumidityChartVisible
-        {
-            get => _isHumidityChartVisible;
-            set
-            {
-                if (value)
-                {
-                    IsTemperatureChartVisible = IsSoilMoistureChartVisible = false;
-                }
-                this.RaiseAndSetIfChanged(ref _isHumidityChartVisible, value);
-            }
-        }
-
-        private bool _isSoilMoistureChartVisible;
-        public bool IsSoilMoistureChartVisible
-        {
-            get => _isSoilMoistureChartVisible;
-            set
-            {
-                if (value)
-                {
-                    IsTemperatureChartVisible = IsHumidityChartVisible = false;
-                }
-                this.RaiseAndSetIfChanged(ref _isSoilMoistureChartVisible, value);
-            }
-        }
-
         private bool _isLightsOn;
         public bool IsLightsOn
         {
@@ -78,12 +36,6 @@ namespace Cultivar_reTerminal.ViewModels
             set => this.RaiseAndSetIfChanged(ref _isSprinklerOn, value);
         }
 
-        public ReactiveCommand<Unit, Unit> ViewTemperatureChartCommand { get; set; }
-
-        public ReactiveCommand<Unit, Unit> ViewHumidityChartCommand { get; set; }
-
-        public ReactiveCommand<Unit, Unit> ViewSoilMoistureChartCommand { get; set; }
-
         public ReactiveCommand<Unit, Unit> ToggleLightsCommand { get; set; }
 
         public ReactiveCommand<Unit, Unit> ToggleHeaterCommand { get; set; }
@@ -98,23 +50,6 @@ namespace Cultivar_reTerminal.ViewModels
 
         public MainWindowViewModel()
         {
-            IsTemperatureChartVisible = true;
-
-            ViewTemperatureChartCommand = ReactiveCommand.Create(() =>
-            {
-                IsTemperatureChartVisible = true;
-            });
-
-            ViewHumidityChartCommand = ReactiveCommand.Create(() =>
-            {
-                IsHumidityChartVisible = true;
-            });
-
-            ViewSoilMoistureChartCommand = ReactiveCommand.Create(() =>
-            {
-                IsSoilMoistureChartVisible = true;
-            });
-
             ToggleLightsCommand = ReactiveCommand.Create(ToggleLights);
 
             ToggleHeaterCommand = ReactiveCommand.Create(ToggleHeater);
