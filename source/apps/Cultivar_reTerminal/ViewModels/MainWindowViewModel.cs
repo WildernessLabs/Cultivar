b6bbcbc6529a82fea97c2f4a820f043c23ae8d69
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-09-20 15:18:06 -0700

    Adjust Oxyplot to show time on the X Axis

--- source/apps/Cultivar_reTerminal/ViewModels/MainWindowViewModel.cs
+++ source/apps/Cultivar_reTerminal/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using Cultivar_reTerminal.Client;
-using OxyPlot;
 using ReactiveUI;
 using System;
 using System.Collections.ObjectModel;
@@ -67,11 +66,11 @@ namespace Cultivar_reTerminal.ViewModels
 
         public ReactiveCommand<Unit, Unit> ToggleSprinklerCommand { get; set; }
 
-        public ObservableCollection<DataPoint> TemperatureLogs { get; set; }
+        public ObservableCollection<Pnl> TemperatureLogs { get; set; }
 
-        public ObservableCollection<DataPoint> HumidityLogs { get; set; }
+        public ObservableCollection<Pnl> HumidityLogs { get; set; }
 
-        public ObservableCollection<DataPoint> SoilMoistureLogs { get; set; }
+        public ObservableCollection<Pnl> SoilMoistureLogs { get; set; }
 
         public MainWindowViewModel()
         {
@@ -83,11 +82,11 @@ namespace Cultivar_reTerminal.ViewModels
 
             ToggleSprinklerCommand = ReactiveCommand.Create(ToggleSprinkler);
 
-            TemperatureLogs = new ObservableCollection<DataPoint>();
+            TemperatureLogs = new ObservableCollection<Pnl>();
 
-            HumidityLogs = new ObservableCollection<DataPoint>();
+            HumidityLogs = new ObservableCollection<Pnl>();
 
-            SoilMoistureLogs = new ObservableCollection<DataPoint>();
+            SoilMoistureLogs = new ObservableCollection<Pnl>();
 
             _ = SimulateCurrentConditions();
         }
@@ -96,10 +95,10 @@ namespace Cultivar_reTerminal.ViewModels
         {
             var random = new Random();
 
-            int i = 0;
-
             while (true)
             {
+                var dateTime = DateTime.Now;
+
                 double temp = random.Next(26, 28) + random.NextDouble();
                 int h = random.Next(95, 97);
                 double sm = random.Next(75, 77) + random.NextDouble();
@@ -108,20 +107,18 @@ namespace Cultivar_reTerminal.ViewModels
                 CurrentHumidity = $"{h}%";
                 CurrentSoilMoisture = $"{sm:N0}%";
 
-                TemperatureLogs.Add(new DataPoint(i, temp));
-                HumidityLogs.Add(new DataPoint(i, h));
-                SoilMoistureLogs.Add(new DataPoint(i, sm));
+                TemperatureLogs.Add(new Pnl(dateTime, temp));
+                HumidityLogs.Add(new Pnl(dateTime, h));
+                SoilMoistureLogs.Add(new Pnl(dateTime, sm));
 
-                await Task.Delay(TimeSpan.FromSeconds(5));
+                await Task.Delay(TimeSpan.FromMinutes(1));
 
-                if (i > 50)
+                if (TemperatureLogs.Count > 10)
                 {
                     TemperatureLogs.RemoveAt(0);
                     HumidityLogs.RemoveAt(0);
                     SoilMoistureLogs.RemoveAt(0);
                 }
-
-                i++;
             }
         }
 
@@ -159,6 +156,23 @@ namespace Cultivar_reTerminal.ViewModels
             {
                 IsSprinklerOn = !IsSprinklerOn;
             }
+        }
+    }
+
+    public class Pnl
+    {
+        public DateTime Time { get; set; }
+        public double Value { get; set; }
+
+        public Pnl(DateTime time, double value)
+        {
+            Time = time;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0:HH:mm} {1:0.0}", this.Time, this.Value);
         }
     }
 }
