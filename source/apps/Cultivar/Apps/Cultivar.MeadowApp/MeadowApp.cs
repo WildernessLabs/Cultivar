f7f402524a1ca0d07eb5d2ddc57708035139c5ce
bryan costanich <b@wowzer.net>
2023-09-19 17:55:21 -0700

    basic project lab functionality in with health logging

--- source/apps/Cultivar/Apps/Cultivar.MeadowApp/MeadowApp.cs
+++ source/apps/Cultivar/Apps/Cultivar.MeadowApp/MeadowApp.cs
@@ -1,27 +1,89 @@
-﻿using Meadow;
+﻿using Cultivar.MeadowApp.UI;
+using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation;
-using Meadow.Foundation.Leds;
-using Meadow.Peripherals.Leds;
+using Meadow.Foundation.Audio;
+using Meadow.Units;
 using System;
 using System.Threading.Tasks;
 
-namespace MeadowApp
+namespace Cultivar.MeadowApp
 {
-    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
-    public class MeadowApp : App<F7FeatherV2>
+    // Change F7FeatherV2 to F7FeatherV1 if using Feather V1 Meadow boards
+    // Change to F7CoreComputeV2 for Project Lab V3.x
+    public class MeadowApp : App<F7CoreComputeV2>
     {
-        RgbPwmLed onboardLed;
+        private DisplayController displayController;
+        private MicroAudio audio;
+        private IProjectLabHardware projLab;
 
         public override Task Initialize()
         {
-            Resolver.Log.Info("Initialize...");
+            Resolver.Log.LogLevel = Meadow.Logging.LogLevel.Trace;
 
-            onboardLed = new RgbPwmLed(
-                redPwmPin: Device.Pins.OnboardLedRed,
-                greenPwmPin: Device.Pins.OnboardLedGreen,
-                bluePwmPin: Device.Pins.OnboardLedBlue,
-                CommonType.CommonAnode);
+            Resolver.Log.Info("Initialize hardware...");
+
+            //==== instantiate the project lab hardware
+            projLab = ProjectLab.Create();
+
+            Resolver.Log.Info($"Running on ProjectLab Hardware {projLab.RevisionString}");
+
+            projLab.RgbLed?.SetColor(Color.Blue);
+
+            projLab.Speaker.SetVolume(0.5f);
+            audio = new MicroAudio(projLab.Speaker);
+
+            //---- display controller (handles display updates)
+            if (projLab.Display is { } display)
+            {
+                Resolver.Log.Trace("Creating DisplayController");
+                displayController = new DisplayController(display);
+                Resolver.Log.Trace("DisplayController up");
+            }
+
+            //---- BH1750 Light Sensor
+            if (projLab.LightSensor is { } bh1750)
+            {
+                bh1750.Updated += Bh1750Updated;
+            }
+
+            //---- BME688 Atmospheric sensor
+            if (projLab.EnvironmentalSensor is { } bme688)
+            {
+                bme688.Updated += Bme688Updated;
+            }
+
+            //---- BMI270 Accel/IMU
+            if (projLab.MotionSensor is { } bmi270)
+            {
+                bmi270.Updated += Bmi270Updated;
+            }
+
+            //---- buttons
+            if (projLab.RightButton is { } rightButton)
+            {
+                rightButton.PressStarted += (s, e) => displayController.RightButtonState = true;
+                rightButton.PressEnded += (s, e) => displayController.RightButtonState = false;
+            }
+
+            if (projLab.DownButton is { } downButton)
+            {
+                downButton.PressStarted += (s, e) => displayController.DownButtonState = true;
+                downButton.PressEnded += (s, e) => displayController.DownButtonState = false;
+            }
+            if (projLab.LeftButton is { } leftButton)
+            {
+                leftButton.PressStarted += (s, e) => displayController.LeftButtonState = true;
+                leftButton.PressEnded += (s, e) => displayController.LeftButtonState = false;
+            }
+            if (projLab.UpButton is { } upButton)
+            {
+                upButton.PressStarted += (s, e) => displayController.UpButtonState = true;
+                upButton.PressEnded += (s, e) => displayController.UpButtonState = false;
+            }
+
+            //---- heartbeat
+            Resolver.Log.Info("Initialization complete");
 
             return base.Initialize();
         }
@@ -30,34 +92,60 @@ namespace MeadowApp
         {
             Resolver.Log.Info("Run...");
 
-            return CycleColors(TimeSpan.FromMilliseconds(1000));
+            _ = audio.PlaySystemSound(SystemSoundEffect.Success);
+
+            //---- BH1750 Light Sensor
+            if (projLab.LightSensor is { } bh1750)
+            {
+                bh1750.StartUpdating(TimeSpan.FromSeconds(5));
+            }
+
+            //---- BME688 Atmospheric sensor
+            if (projLab.EnvironmentalSensor is { } bme688)
+            {
+                bme688.StartUpdating(TimeSpan.FromSeconds(5));
+            }
+
+            //---- BMI270 Accel/IMU
+            if (projLab.MotionSensor is { } bmi270)
+            {
+                bmi270.StartUpdating(TimeSpan.FromSeconds(5));
+            }
+
+            displayController?.Update();
+
+            Resolver.Log.Info("starting blink");
+            _ = projLab.RgbLed.StartBlink(WildernessLabsColors.PearGreen, TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(2000), 0.5f);
+
+            return base.Run();
         }
 
-        async Task CycleColors(TimeSpan duration)
-        {
-            Resolver.Log.Info("Cycle colors...");
 
-            while (true)
+        private void Bmi270Updated(object sender, IChangeResult<(Acceleration3D? Acceleration3D, AngularVelocity3D? AngularVelocity3D, Temperature? Temperature)> e)
+        {
+            Resolver.Log.Info($"BMI270: {e.New.Acceleration3D.Value.X.Gravity:0.0},{e.New.Acceleration3D.Value.Y.Gravity:0.0},{e.New.Acceleration3D.Value.Z.Gravity:0.0}g");
+            if (displayController != null)
             {
-                await ShowColorPulse(Color.Blue, duration);
-                await ShowColorPulse(Color.Cyan, duration);
-                await ShowColorPulse(Color.Green, duration);
-                await ShowColorPulse(Color.GreenYellow, duration);
-                await ShowColorPulse(Color.Yellow, duration);
-                await ShowColorPulse(Color.Orange, duration);
-                await ShowColorPulse(Color.OrangeRed, duration);
-                await ShowColorPulse(Color.Red, duration);
-                await ShowColorPulse(Color.MediumVioletRed, duration);
-                await ShowColorPulse(Color.Purple, duration);
-                await ShowColorPulse(Color.Magenta, duration);
-                await ShowColorPulse(Color.Pink, duration);
+                displayController.AccelerationConditions = e.New;
             }
         }
 
-        async Task ShowColorPulse(Color color, TimeSpan duration)
+        private void Bme688Updated(object sender, IChangeResult<(Temperature? Temperature, RelativeHumidity? Humidity, Pressure? Pressure, Resistance? GasResistance)> e)
         {
-            await onboardLed.StartPulse(color, duration / 2);
-            await Task.Delay(duration);
+            Resolver.Log.Info($"BME688: {(int)e.New.Temperature?.Celsius}C - {(int)e.New.Humidity?.Percent}% - {(int)e.New.Pressure?.Millibar}mbar");
+            if (displayController != null)
+            {
+                displayController.AtmosphericConditions = e.New;
+            }
+        }
+
+        private void Bh1750Updated(object sender, IChangeResult<Illuminance> e)
+        {
+            Resolver.Log.Info($"BH1750: {e.New.Lux}");
+            if (displayController != null)
+            {
+                displayController.LightConditions = e.New;
+            }
         }
     }
 }
