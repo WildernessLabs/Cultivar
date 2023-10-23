996ced9a4c50dded2a2d606b2d5ca8edb3901264
bryan costanich <b@wowzer.net>
2023-10-23 11:57:01 -0600

    adding a 3hr restart to workaround HTTP stability issues.

--- source/apps/Cultivar/Apps/Cultivar.MeadowApp/MeadowApp.cs
+++ source/apps/Cultivar/Apps/Cultivar.MeadowApp/MeadowApp.cs
@@ -3,13 +3,18 @@ using Cultivar.Hardware;
 using Meadow;
 using Meadow.Devices;
 using Meadow.Logging;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cultivar.MeadowApp
 {
     public class MeadowApp : App<F7CoreComputeV2>
     {
-        private GreenhouseController greenhouseController;
+        GreenhouseController greenhouseController;
+        int WatchdogUptimeMaxHours = 3;
+        int WatchdogUptimePetCountMax = 0;
+        int WatchdogCount = 0;
 
         public override Task Initialize()
         {
@@ -31,9 +36,39 @@ namespace Cultivar.MeadowApp
         {
             Resolver.Log.Info("Run...");
 
+            // Enable the watchdog for 30 second intervals (max is ~32s)
+            Device.WatchdogEnable(TimeSpan.FromSeconds(30));
+            // calculate the number of times we need to pet the watchdog.
+            WatchdogUptimePetCountMax = ((WatchdogUptimeMaxHours * 60 * 60) / 30);
+            // Start the thread that resets the counter.
+            StartPettingWatchdog(TimeSpan.FromSeconds(9));
+
             greenhouseController.Run();
 
             return base.Run();
+        }
+
+        void StartPettingWatchdog(TimeSpan pettingInterval)
+        {
+            // Just for good measure, let's reset the watchdog to begin with.
+            Device.WatchdogReset();
+            // Start a thread that restarts it.
+            Thread t = new Thread(async () => {
+                while (true)
+                {
+                    if (WatchdogCount <= WatchdogUptimePetCountMax) {
+                        Thread.Sleep(pettingInterval);
+                        Console.WriteLine("Petting watchdog.");
+                        Device.WatchdogReset();
+                    } else
+                    {
+                        // stop spinning while it restarts
+                        await Task.Delay(1000);
+                    }
+                    WatchdogCount++;
+                }
+            });
+            t.Start();
         }
     }
 }
