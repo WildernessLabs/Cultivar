d9fa6ca2e8312c8b84dda2e461222b027390ba7d
Adrian Stevens <adrian.stevens@gmail.com>
2023-10-22 21:38:45 -0700

    Cleanup simulated display loading

--- source/apps/Cultivar/Apps/Cultivar.Simulator/Views/MainWindow.axaml.cs
+++ source/apps/Cultivar/Apps/Cultivar.Simulator/Views/MainWindow.axaml.cs
@@ -8,14 +8,10 @@ namespace ProjectLabSimulator.Views
 {
     public partial class MainWindow : Window
     {
-        private GreenhouseController greenhouseController;
-        SimulatedHardware greenhouseHardware;
-
-        PixelCanvas canvas;
+        private readonly GreenhouseController greenhouseController;
+        readonly SimulatedHardware greenhouseHardware;
 
         readonly int scale = 2;
-
-        readonly ISimulatedDisplay simDisplay;
 
         public MainWindowViewModel ViewModel { get; }
 
@@ -23,21 +19,21 @@ namespace ProjectLabSimulator.Views
         {
             InitializeComponent();
 
-            greenhouseHardware = new SimulatedHardware();
-            simDisplay = new Ili9341Simulated();
-
-            ReloadCanvas();
+            greenhouseHardware = new SimulatedHardware()
+            {
+                Display = LoadDisplay()
+            };
 
             greenhouseController = new GreenhouseController(greenhouseHardware, true);
 
             greenhouseController.Run();
         }
 
-        void ReloadCanvas()
+        PixelCanvas LoadDisplay()
         {
-            displayBorder.Child = null;
+            var simDisplay = new Ili9341Simulated();
 
-            canvas = new PixelCanvas(simDisplay.Width, simDisplay.Height, simDisplay.ColorMode)
+            var canvas = new PixelCanvas(simDisplay.Width, simDisplay.Height, simDisplay.ColorMode)
             {
                 Width = simDisplay.Width * scale,
                 Height = simDisplay.Height * scale,
@@ -47,19 +43,10 @@ namespace ProjectLabSimulator.Views
                 DisabledColor = simDisplay.BackgroundColor,
             };
 
-            //hacky ... ToDo
-            greenhouseHardware.Display = canvas;
-
-
             displayBorder.Child = canvas;
             displayBorder.Width = canvas.Width;
 
-        }
-
-        public override void Show()
-        {
-            canvas?.InvalidateVisual();
-            base.Show();
+            return canvas;
         }
     }
 }
